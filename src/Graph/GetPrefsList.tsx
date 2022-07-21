57491fbbfa13f4ccb64330fb8d9b03f3f2551368
larcenciel618 <hhiirroossee816@gmail.com>
2022-07-21 15:33:53 +0900

    複数のグラフの描画と、チェックが外された時にグラフの消去する機能を実装しました
    複数のグラフを加えたり消したり繰り返すと、二つの都道府県のグラフが完全に重なるというバグがあります

--- src/Graph/GetPrefsList.tsx
+++ src/Graph/GetPrefsList.tsx
@@ -1,63 +1,46 @@
 import React, { useEffect , useState } from "react";
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-
-interface PrefArray {
-	prefCode: number[];
-	prefName: string[];
-}
+import { setUncaughtExceptionCaptureCallback } from "process";
 
 interface PrefValue {
 	prefCode: number;
-	prefName: string[];
-}
-
-interface PrefDataArrayType {
-	PrefDataArray: {
-		year: number;
-		value: number;
-	}[];
+	prefName: string;
 }
 
 const GetPrefsList = () => {
 	const[Prefs, setPrefs] = useState([]);
 	const[Populations, setPopulations] = useState<number[]>([]);
-	const[Years, setYears] = useState<number[]>([]);
-	// const [PrefData, setPrefData] = useState<PrefDataArrayTypae>({
-	// 	PrefDataArray: {0, 0}, {0, 0}
-	// 	}
-	// });
-	const	categories:any = [];
-	// const[PrefData, setPrefData] = useState<PrefDataArrayType>({PrefData: newArray(18).fill(0, 0)});
-	// const[Prefs, setPrefs] = useState<PrefArray>({Prefs: new Array(47).fill(0, null)});
-	// const[PrefData, setPrefData] = useState<PrefDataArray>({
-	// 	year:  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	// 	value: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	// });
+	const[SeriesList, setSeriesList] = useState<Highcharts.SeriesOptionsType[]>([]);
 	const[isDrew, reverseIsDrew] = useState(true);
 
-	const handleClick = (index: number) => {
+	const handleClick = (index: number, prefname: string) => {
 		//apiでチェックボックスから受け取ったindex番目の都道府県のデータを取得しています。
 		fetch("https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=" + index, {
 			headers: {
-			   "x-api-key": "5z07nougGOxYPvf26JK69uVftaVNc00AuZPmbnN9"
-		    }
+			"x-api-key": "5z07nougGOxYPvf26JK69uVftaVNc00AuZPmbnN9"
+			}
 		})
 		.then(res => res.json())
 		.then(data => {
-			let years: number[] = [];
-			//apiで受け取ったデータの年数リストを宣言した配列yearsに格納してます
-			Object.keys(data.result.data[0].data).forEach((value) => {
-				years.push(data.result.data[0].data[value].value);
-				setYears(years);
+			let currentValues: number[] = [];
+			//apiで受け取ったデータの人口リストを宣言した配列currentValuesに格納してます
+			Object.keys(data.result.data[0].data).forEach((list: string) => {
+				currentValues.push(data.result.data[0].data[list].value);
+				setPopulations(currentValues);
 			});
-			let values: number[] = [];
-			//apiで受け取ったデータの人口リストを宣言した配列valuesに格納してます
-			Object.keys(data.result.data[0].data).forEach((value) => {
-				values.push(data.result.data[0].data[value].value);
-				setPopulations(values);
-				setYears(years);
+			const currentSeries: Highcharts.SeriesOptionsType = {type: "line",name: prefname, data: currentValues};
+			//SeriesList内nameを全て検索し、該当する要素があった場合は、isExistedにtrueをたてる
+			let isExisted: boolean = false;
+			SeriesList.forEach((type: Highcharts.SeriesOptionsType) => {
+				if (type.name === prefname){
+					isExisted = true;
+				}
 			});
+			//該当する要素があった(checkbuttonが外れた)場合は現在のSetseriesから該当する要素を抜いたリストを、なかった場合は要素を付け加えたリストをセットする
+			isExisted
+			? setSeriesList(SeriesList.filter(list => list.name !== prefname))
+			: setSeriesList([...SeriesList, currentSeries])
 		})
 	}
 
@@ -87,7 +70,6 @@ const GetPrefsList = () => {
 		  },
 		  categories: ['1960', '1965', '1970', '1975', '1980', '1985', '1990', '1995', '2000',
 		  				'2005', '2010', '2015', '2020', '2025', '2030', '2035', '2040', '2045'],
-		//   categories: Years.toString(),
 		},
 		yAxis: {
 		  title: {
@@ -101,18 +83,21 @@ const GetPrefsList = () => {
 				},
 			}
 		},
-		// 都道府県を一つも選んでいない場合との分岐条件
-		series: [{ type: "line", name: "都道府県名", data: Populations}],
+		series:  SeriesList.length === 0
+			? [{type: "line", name: "都道府県名", data: []}]
+			: SeriesList,
 	};
 	return (
 		<>
 			<div className="GetPrefList">
-				<div className="Prefboxlist">{Prefs.map((data: PrefValue) => (
-					<div className="Singleset" key={data.prefCode}>
-						<input type="checkbox" onClick={() => handleClick(data.prefCode)}/>
-						<div className="Prefname">{data.prefName}</div>
-					</div>
-				))}</div>
+				<div className="Prefboxlist">
+					{Prefs.map((data: PrefValue) => (
+						<div className="Singleset" key={data.prefCode}>
+							<input type="checkbox" onClick={() => handleClick(data.prefCode, data.prefName)}/>
+							<div className="Prefname">{data.prefName}</div>
+						</div>
+					))}
+				</div>
 			</div>
 			<div className="GraphArea">
 				<HighchartsReact highcharts={Highcharts} options={options} />
